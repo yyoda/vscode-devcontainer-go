955a5cc58c788000f12bbe0a4ff99a1f26f1ed69
Yusuke Yoda <y.yoda@gloops.com>
2019-12-25 09:51:50 +0000

    sample implementation.

---
+++ internal/core/b.go
@@ -0,0 +1,8 @@
+package core
+
+import "fmt"
+
+// Exec2 aaaaa
+func Exec2() {
+	fmt.Println("xxxxxxxxxxx")
+}
---
+++ internal/core/e.go
@@ -0,0 +1,8 @@
+package core
+
+import "fmt"
+
+// Exec aaaaa
+func Exec() {
+	fmt.Println("tttttttttttt")
+}
